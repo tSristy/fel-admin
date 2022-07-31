f9910f4929df8f084b5b37ff69748003f121a033
tSristy <hoque.tasnim05@gmail.com>
2022-07-31 18:08:42 +0600

    last

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Home.css';
 import HomeHeader from '../HomeHeader/HomeHeader';
+import HomeNav from '../HomeNav/HomeNav';
 
 const Home = () => {
     return (
         <>
             <HomeHeader></HomeHeader>
+            <HomeNav></HomeNav>
         </>
     );
 };
--- src/Components/HomeNav/HomeNav.js
+++ src/Components/HomeNav/HomeNav.js
@@ -4,7 +4,12 @@ import './HomeNav.css';
 const HomeNav = () => {
     return (
         <div>
-            
+            <div className='test1'>
+                hudai
+            </div>
+            <div className='test2'>
+                hudai 2222222
+            </div>
         </div>
     );
 };
